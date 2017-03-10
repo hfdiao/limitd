d1b22f768c36cff9f6fdb7f1a6c879e06534ec81
José F. Romaniello <jfromaniello@gmail.com>
2017-03-10 10:26:24 -0300

    refactor perf test

--- perf/same_take.js
+++ perf/same_take.js
@@ -5,103 +5,58 @@ const _            = require('lodash');
 const Stats        = require('fast-stats').Stats;
 const Table        = require('cli-table');
 const ProgressBar  = require('progress');
-const cluster      = require('cluster');
-const LimitdServer = require('../server');
-const profiler     = require('v8-profiler');
-const fs           = require('fs');
+const spawn        = require('child_process').spawn;
 
-if (cluster.isMaster) {
-  process.title = 'limitd server';
-
-  const run_with_profile = process.argv.indexOf('--profile') > -1;
-
-  const db_file = path.join(__dirname, 'db', 'perf.tests.db');
-
-  if (run_with_profile) {
-    console.log('starting profiler');
-    profiler.startProfiling('1', true);
-  }
-
-  // var fs = require('fs');
-  // var profile1 = profiler.stopProfiling();
-  // profiler.startProfiling('2', true);
-  // var profile2 = profiler.stopProfiling();
-
-  // console.log(snapshot1.getHeader(), snapshot2.getHeader());
-
-  // profile1.export(function(error, result) {
-  //   fs.writeFileSync('profile1.json', result);
-  //   profile1.delete();
-  // });
+(function spawn_server() {
+  console.log('spawning server');
 
   try{
+    const db_file = path.join(__dirname, 'db', 'perf.tests.db');
     rimraf.sync(db_file);
   } catch(err){}
 
-  var server = new LimitdServer({
-    db:        db_file,
-    log_level: 'error',
-    buckets: {
-      ip: {
-        size: 1000,
-        override: {
-          '10.0.0.1': {
-            size: 1000,
-            unlimited: true
-          }
-        }
-      }
-    }
+  const limitd_args = ['--config-file', 'config.yml'];
+
+  const run_with_profile = process.argv.indexOf('--profile') > -1;
+
+  if (run_with_profile) {
+    limitd_args.push('--profile');
+  }
+
+  const server = spawn(`${path.normalize(__dirname + '/../bin/limitd')}`, limitd_args, { stdio: 'inherit' });
+
+  process.once('exit', (code) => {
+    server.kill();
+    process.exit(code);
   });
+})();
 
-
-  server.start(function (err, address) {
-    if (err) {
-      console.error(err.message);
-      return process.exit(1);
-    }
-    cluster.fork({LIMITD_HOST: `limitd://${address.address}:${address.port}`});
-  });
-
-  cluster.on('exit', (worker, code) => {
-    if (!run_with_profile) {
-      return process.exit(code);
-    }
-
-    var profile = profiler.stopProfiling();
-
-    profile.export()
-      .pipe(fs.createWriteStream(`${Date.now()}.cpuprofile`))
-      .on('finish', function() {
-        profile.delete();
-        process.exit(code);
-      });
-  });
-
-  return;
-}
-
-process.title = 'limitd client';
+process.title = 'limitd perfomance client';
 
 const LimitdClient = require('limitd-client');
 
-const client = new LimitdClient({ timeout: '60s', host: process.env.LIMITD_HOST });
+const clients = _.range(0, 10).map(() => new LimitdClient({host: 'limitd://localhost:19001', timeout: 60000 }));
 
-client.once('ready', run_tests);
+clients.forEach(c => c.once('ready', waitAll));
 
+function waitAll(){
+  if(clients.every(c => c.socket && c.socket.connected)){
+    run_tests();
+  }
+}
 
 function run_tests () {
-  var client = this;
-  var started_at = new Date();
+  const started_at = new Date();
 
-  var requests = 200000;
-  var concurrency = 5000;
+  const requests = 200000;
+  const concurrency = 5000;
 
-  var progress = new ProgressBar(':bar', { total: requests , width: 50 });
+  const progress = new ProgressBar(':bar', { total: requests , width: 50 });
 
   async.mapLimit(_.range(requests), concurrency, function (i, done) {
-    var date = new Date();
-
+    const date = new Date();
+    const client = clients.shift();
+    clients.push(client);
     return client.take('ip', '10.0.0.1', function (err, result) {
       progress.tick();
       if (err) {
